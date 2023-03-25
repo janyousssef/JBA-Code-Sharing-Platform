92b9e1eb60d34eebbd9c9e01f7b6c81465ecef58
Jan Youssef <janyoussef19@gmail.com>
2023-03-25 09:48:46 +0200

    add /code and /api/code endpoints, both return the same piece of code

--- src/main/java/com/jan/codesharing/api/ApiController.java
+++ src/main/java/com/jan/codesharing/api/ApiController.java
@@ -1,5 +1,6 @@
 package com.jan.codesharing.api;
 
+import com.jan.codesharing.code.CodeSnippet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api", produces = "application/json")
 public class ApiController {
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("hello from api", HttpStatus.OK);
+    @GetMapping("/code")
+    public ResponseEntity<?> hello() {
+        CodeSnippet codeSnippet = new CodeSnippet("""
+                public static void main(String[] args) {
+                   \s
+                }
+                """);
+        return new ResponseEntity<>(codeSnippet, HttpStatus.OK);
     }
+
+
 }
--- src/main/java/com/jan/codesharing/web/WebController.java
+++ src/main/java/com/jan/codesharing/web/WebController.java
@@ -1,7 +1,5 @@
 package com.jan.codesharing.web;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(path = "/",produces = "text/html")
 public class WebController {
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello(){
-        return new ResponseEntity<>("hello from web", HttpStatus.OK);
+    @GetMapping("/code")
+    public String hello(){
+        System.out.println("here");
+        return "code";
     }
 }

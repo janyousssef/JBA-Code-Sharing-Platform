579e3ea2160c028c96d46ecb4a28be719be03628
Jan Youssef <janyoussef19@gmail.com>
2023-03-25 09:19:59 +0200

    all responses from api controlled will now have `Content-Type=application/json` header

--- src/main/java/com/jan/codesharing/api/ApiController.java
+++ src/main/java/com/jan/codesharing/api/ApiController.java
@@ -1,14 +1,16 @@
 package com.jan.codesharing.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path ="/api")
+@RequestMapping(path = "/api", produces = "application/json")
 public class ApiController {
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("hello", HttpStatus.OK);
     }
 }

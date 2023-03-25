1426eaa407a9df0093f4aaeed880fb021ef7e7a7
Jan Youssef <janyoussef19@gmail.com>
2023-03-25 14:13:53 +0200

    refactored the HTML page to use thymeleaf templates

--- src/main/java/platform/web/WebController.java
+++ src/main/java/platform/web/WebController.java
@@ -3,13 +3,18 @@ package platform.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ModelAndView;
+import platform.code.CodeEntity;
 
 @Controller
 @RequestMapping(path = "/",produces = "text/html")
 public class WebController {
     @GetMapping("/code")
-    public String hello(){
-        return "code.html";
+    public ModelAndView getCode(){
+        CodeEntity codeSnippet = new CodeEntity("public static void main(String[] args) {}");
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("codeEntity",codeSnippet);
+        mv.setViewName("code");
+        return mv;
     }
 }

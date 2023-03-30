c5aaf785f8d8c14a37369f5563522f16c1109eee
Jan Youssef <janyoussef19@gmail.com>
2023-03-30 17:46:04 +0200

    refactor

--- src/main/java/platform/code/CodeRepo.java
+++ src/main/java/platform/code/CodeRepo.java
@@ -7,13 +7,13 @@ import java.time.format.DateTimeFormatter;
 
 @Repository
 public class CodeRepo {
-    private CodeEntity code = new CodeEntity("public static void main(String[] args) {}", getDate());
+    String initialCode = "public static void main(String[] args) {}";
+    private CodeEntity code = new CodeEntity(initialCode, getDate());
 
     public CodeRepo() {
     }
 
     private static String getDate() {
-
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return LocalDateTime.now().format(formatter);
     }

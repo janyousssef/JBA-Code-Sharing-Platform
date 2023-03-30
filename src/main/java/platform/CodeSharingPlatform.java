763aba035a08d8a0b1c0a07196923cf00cb6b464
Jan Youssef <janyoussef19@gmail.com>
2023-03-30 17:42:30 +0200

    format code

--- src/main/java/platform/CodeSharingPlatform.java
+++ src/main/java/platform/CodeSharingPlatform.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodeSharingPlatform {
     public static void main(String[] args) {
-    SpringApplication.run(CodeSharingPlatform.class, args);
+        SpringApplication.run(CodeSharingPlatform.class, args);
     }
 }
